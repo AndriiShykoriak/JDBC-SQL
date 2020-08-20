9d4a733759d74dec595e21777e5e4eee8d00f982
AndriiShykoriak <andrii.shykoriak@gmail.com>
2020-08-20 17:57:26 +0300

    main.class

--- src/main/java/com/ua/jdbc/Application.java
+++ src/main/java/com/ua/jdbc/Application.java
@@ -1,9 +1,9 @@
 package com.ua.jdbc;
 
-import com.ua.jdbc.database.Database;
+import com.ua.jdbc.database.Operations;
 
 public class Application {
     public static void main(String[] args) {
-        new Database().createDB();
+        new Operations().createDB();
     }
 }
